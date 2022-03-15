a7b0c94399297abf28d02fa109ad71b768bde5d7
Stefan Watt <stefan.watt@gmail.com>
2022-03-15 15:14:17 +0100

    alyx i3

---
+++ bin/autostart.sh
@@ -0,0 +1,64 @@
+#!/bin/env bash
+
+# sets wallpaper using feh
+# bash $HOME/.config/i3/.fehbg
+# set proper refrsh rate etc.
+bash ~/.config/.xrandr.sh &
+
+feh --randomize --bg-fill ~/.wallpapers/* &
+
+/usr/bin/variety &
+# polybar
+$HOME/.config/i3/bin/launchbar.sh
+
+# Fix cursor
+xsetroot -cursor_name left_ptr
+
+# kill if already running
+killall -9 picom xfce4-power-manager ksuperkey dunst sxhkd conky eww
+
+# Launch Conkeww
+# sed -i "s/colors\/color-.*/colors\/color-nord.yuck\")/g" $HOME/.config/conkeww/eww.yuck
+# eww --config $HOME/.config/conkeww/ open conkeww-main
+# 
+# Launch Conky
+# conky -c $HOME/.config/conky/axyl.conkyrc
+
+# sets superkey
+ksuperkey -e 'Super_L=Alt_L|F1' &
+ksuperkey -e 'Super_R=Alt_L|F1' &
+
+# start hotkey daemon
+sxhkd &
+
+# Launch notification daemon
+dunst -config $HOME/.config/i3/dunstrc &
+
+# start compositor and power manager
+xfce4-power-manager &
+
+autokey-gtk
+
+while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+picom --config $HOME/.config/i3/picom.conf &
+
+# start polkit
+if [[ ! `pidof xfce-polkit` ]]; then
+    /usr/lib/xfce-polkit/xfce-polkit &
+fi
+
+# start udiskie
+udiskie &
+
+# replace neovim colorscheme
+sed -i "s/theme =.*$/theme = \"norchad\",/g" $HOME/.config/nvim/lua/chadrc.lua
+
+# change xfce4-terminal colorscheme
+XFCE_TERM_PATH="$HOME/.config/xfce4/terminal"
+cp "$XFCE_TERM_PATH"/colorschemes/nord "$XFCE_TERM_PATH"/terminalrc
+
+#bluetooth
+blueman-applet &
+
+# set proper touchpad acceleration
+bash ~/.config/.touchpad.sh &
